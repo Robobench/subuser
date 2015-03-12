6411b5ee8bb3842ae1018b8bf754171a0de5d965
jweisz <jweisz@katic.(none)>
2015-03-12 03:44:15 -0400

    removed redundant apt-gets inside nvidia install part

--- logic/dockerside-scripts/robobench/install_client_gpu.bash
+++ logic/dockerside-scripts/robobench/install_client_gpu.bash
@@ -18,11 +18,6 @@ if [ $has_nvidia ]; then
 	nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${nvidia_version}/NVIDIA-Linux-x86_64-${nvidia_version}.run
 	wget -O $nvidia_bin $nvidia_driver_uri
     fi
-    apt-get clean
-    apt-get update
-    apt-get -y install --force-yes xserver-xorg-core x-window-system libgl1-mesa-glx
-    apt-get install -y binutils mesa-utils module-init-tools
-
     sh $nvidia_bin -a -N --ui=none --no-kernel-module -q
 
     rm $nvidia_bin
